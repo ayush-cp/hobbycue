11616493d80ba0f0d18b53b5db26b892fa4f7c3f
Ayush Panwar <ayushpanwarcp92@gmail.com>
2024-12-02 10:11:12 +0530

    initialising repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './Components/Home'
+
+function App() {
+
+  return (
+    <div className='w-full min-h-screen h-max bg-gray-400'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
