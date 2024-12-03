2d25dad844f00275f5fdbbdb09dfb2049706b38a
wbidit <brb020@nist.edu>
2024-12-03 12:55:19 +0530

    minor fixes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Hero from "./Hero";
 import AddYourOwnSection from "./AddYourOwn";
 import Categories from "./Categories";
-import AddYourOwnSection from "./AddYourOwn";
+import HobbyAndCommunity from "./HobbyAndCommunity";
 import TestimonialSection from "./testimonials";
 
 const Home = () => {
   return (
     <div className="w-full h-full flex flex-col gap-4">
       <Hero />
-      <HobbyAndCommunity />
+      
+  
       <Categories />
       <AddYourOwnSection />
       <TestimonialSection />
+      <HobbyAndCommunity />
     </div>
   );
 };
