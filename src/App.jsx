2c8d0427c2b5dc3cd5df1ef3b28641592edab13d
Shashank MS <shashupreethims@gmail.com>
2024-12-03 12:10:24 +0530

    m3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import Home from './Components/Home'
-import Footer from './Components/footer';
-import Navbar from './Components/navbar';
-import AddYourOwnSection from './Components/AddYourOwn';
-import TestimonialSection from './Components/testimonials';
+import Home from "./Components/Home";
+import Footer from "./Components/footer";
+import Navbar from "./Components/navbar";
+import React from "react";
 function App() {
   return (
-    <div className='w-full min-h-screen h-max bg-white-400'>
+    <div className="w-full min-h-screen h-max bg-white-400">
       <Navbar />
       <Home />
-      <AddYourOwnSection />
-      <TestimonialSection />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Hero from './Hero'
-import Navbar from './navbar'
+import React from "react";
+import Hero from "./Hero";
+import AddYourOwnSection from "./AddYourOwn";
+import TestimonialSection from "./testimonials";
+import HobbyAndCommunity from "./HobbyAndCommunity";
 
 const Home = () => {
   return (
-    <div className='w-full h-full flex flex-col gap-4'>
-        <Navbar/>
-        <Hero/>
-       
+    <div className="w-full h-full flex flex-col gap-4">
+      <Hero />
+      <AddYourOwnSection />
+      <TestimonialSection />
+      <HobbyAndCommunity />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
